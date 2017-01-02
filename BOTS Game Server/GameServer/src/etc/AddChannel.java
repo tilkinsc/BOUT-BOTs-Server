74c44d49721a4dbed49649876997869e2e45dfba
bud <bud@BudPc>
2017-01-02 14:00:03 -0500

    Specified MySQL macro, created MySQL macro for channels

---
+++ BOTS Game Server/GameServer/src/etc/AddChannel.java
@@ -0,0 +1,29 @@
+package etc;
+
+import java.net.UnknownHostException;
+
+import shared.SQLDatabase;
+
+public class AddChannel {
+
+	// Adds a user to the database...
+	public static void main(String[] args) throws UnknownHostException {
+		
+		final int CHANNEL_ID = 2;
+		final String CHANNEL_NAME = "Beta Test";
+		final int CHANNEL_MIN_LEVEL = 10;
+		final int CHANNEL_MAX_LEVEL = 255;
+		final int PLAYERS = 0;
+		final int STATUS = 1;
+		
+		final String query = "INSERT INTO `bout_channels` (id, name, minlevel, maxlevel, players, status) VALUES "
+				+ "(" + CHANNEL_ID + ", \"" + CHANNEL_NAME + "\", " + CHANNEL_MIN_LEVEL + ", " + CHANNEL_MAX_LEVEL + ", "
+				+ PLAYERS + ", " + STATUS + ")";
+
+		final SQLDatabase db = new SQLDatabase("");
+		db.start();
+		db.doupdate(query);
+
+	}
+
+}
--- BOTS Game Server/GameServer/src/etc/AddCharacter.java
+++ BOTS Game Server/GameServer/src/etc/AddCharacter.java
@@ -1,5 +1,6 @@
 package etc;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -10,7 +11,7 @@ public class AddCharacter {
 
 	// Adds a user to the database...
 	public static void main(String[] args) throws UnknownHostException {
-		
+
 		final int LOGIN_ID = 0;
 		final int COINS = 1000;
 		final String LOGIN_USERNAME = "ydroque";
@@ -18,58 +19,56 @@ public class AddCharacter {
 		final int LOGIN_BANNED = 0;
 		final int LOGIN_ALLOG = 0;
 		final int LOGIN_COUNT = 0;
-		final String local_ip = java.net.Inet4Address.getLocalHost().toString();
+		final String local_ip = InetAddress.getLocalHost().toString();
 		final String LOGIN_IP = local_ip.substring(0, 0) + local_ip.substring(1);
 		final String LOGIN_EMAIL = "name@site.com";
-		
-		
+
 		final java.util.Date date = new java.util.Date();
-		java.text.SimpleDateFormat dformat = new java.text.SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
-		
+		final java.text.SimpleDateFormat dformat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 		final String query = "INSERT INTO `bout_users` (id, username, password, coins, banned, online, current_ip, logincount, last_ip, lastlogin, email)"
-				+ "VALUES ("
-				+ LOGIN_ID + ",\"" + LOGIN_USERNAME + "\",\"" + LOGIN_PASSWORD + "\"," + COINS + "," + LOGIN_BANNED + "," + LOGIN_ALLOG + ",\"" + LOGIN_IP + "\"," + LOGIN_COUNT + ",\"" + LOGIN_IP + "\",'" + dformat.format(date) + "',\"" + LOGIN_EMAIL + "\");";
-		
+				+ "VALUES (" + LOGIN_ID + ",\"" + LOGIN_USERNAME + "\",\"" + LOGIN_PASSWORD + "\"," + COINS + ","
+				+ LOGIN_BANNED + "," + LOGIN_ALLOG + ",\"" + LOGIN_IP + "\"," + LOGIN_COUNT + ",\"" + LOGIN_IP + "\",'"
+				+ dformat.format(date) + "',\"" + LOGIN_EMAIL + "\");";
+
 		final SQLDatabase db = new SQLDatabase("");
 		db.start();
 		db.doupdate(query);
-		
-	}
-	
-    private static String md5hash(String text) {
-        try {
-        MessageDigest md = null;
-        byte[] encryptMsg = null;
-        try {
-             md = MessageDigest.getInstance("MD5");
-             encryptMsg = md.digest(text.getBytes("ISO8859-1"));
-        }
-        catch (NoSuchAlgorithmException e) {
-        }
-        String swap = "";
-        String byteStr = "";
-        StringBuffer strBuf = new StringBuffer();
-        for (int i = 0; i <= encryptMsg.length - 1; i++) {
-            byteStr = Integer.toHexString(encryptMsg[i]);
-            switch (byteStr.length()) {
-                case 1:
-                    swap = "0" + Integer.toHexString(encryptMsg[i]);
-                    break;
-                case 2:
-                    swap = Integer.toHexString(encryptMsg[i]);
-                    break;
-                case 8:
-                    swap = (Integer.toHexString(encryptMsg[i])).substring(6, 8);
-                    break;
-            }
-            strBuf.append(swap);
-        }
-        String hash = strBuf.toString();
-        return hash;
-        } catch(Exception e){
 
-        }
-        return null;
-     }
-	
+	}
+
+	private static String md5hash(String text) {
+		try {
+			byte[] encryptMsg = null;
+			try {
+				final MessageDigest md = MessageDigest.getInstance("MD5");
+				encryptMsg = md.digest(text.getBytes("ISO8859-1"));
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			String swap = "";
+			final StringBuffer strBuf = new StringBuffer();
+			for (int i = 0; i <= encryptMsg.length - 1; i++) {
+				switch (Integer.toHexString(encryptMsg[i]).length()) {
+				case 1:
+					swap = "0" + Integer.toHexString(encryptMsg[i]);
+					break;
+				case 2:
+					swap = Integer.toHexString(encryptMsg[i]);
+					break;
+				case 8:
+					swap = (Integer.toHexString(encryptMsg[i])).substring(6, 8);
+					break;
+				}
+				strBuf.append(swap);
+			}
+			return strBuf.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+	}
+
 }
