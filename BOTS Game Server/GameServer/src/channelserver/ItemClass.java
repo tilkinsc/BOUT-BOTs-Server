2914161ba6ab2ff2c8ae0757fa82b921d4efb95e
bud <bud@BudPc>
2017-01-21 05:56:37 -0500

    removed unused test variable and fixed indentation before I go mad

--- BOTS Game Server/GameServer/src/channelserver/ItemClass.java
+++ BOTS Game Server/GameServer/src/channelserver/ItemClass.java
@@ -6,6 +6,7 @@ import shared.SQLDatabase;
 
 public class ItemClass {
 
+	
 	public String getItemName(int id) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT name FROM bout_items WHERE id=" + id + " LIMIT 1");
@@ -17,7 +18,7 @@ public class ItemClass {
 		}
 		return null;
 	}
-
+	
 	public int getItemId(String name) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT id FROM bout_items WHERE name='" + name + "' LIMIT 1");
@@ -29,7 +30,7 @@ public class ItemClass {
 		}
 		return 0;
 	}
-
+	
 	public String[] getItemIdLike(String name) {
 		try {
 			int i = 0;
@@ -49,20 +50,19 @@ public class ItemClass {
 		}
 		return null;
 	}
-
+	
 	public ResultSet getItemShopInfos(int id) {
 		try {
-			final String ret = new String();
 			final ResultSet rs = SQLDatabase.doquery("SELECT * FROM bout_items WHERE id=" + id + " LIMIT 1");
 			if (rs.next())
 				return rs;
 			return null;
 		} catch (Exception e) {
-
+			
 		}
 		return null;
 	}
-
+	
 	public int getSell(int id) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT sell FROM bout_items WHERE id=" + id + " LIMIT 1");
@@ -70,11 +70,11 @@ public class ItemClass {
 				return rs.getInt("sell");
 			return -1;
 		} catch (Exception e) {
-
+			
 		}
 		return -1;
 	}
-
+	
 	public int getBuy(int id) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT buy, buyable FROM bout_items WHERE id=" + id + " LIMIT 1");
@@ -83,11 +83,11 @@ public class ItemClass {
 					return rs.getInt("buy");
 			return -1;
 		} catch (Exception e) {
-
+			
 		}
 		return -1;
 	}
-
+	
 	public int getBuyCoins(int id) {
 		try {
 			final ResultSet rs = SQLDatabase
@@ -97,11 +97,11 @@ public class ItemClass {
 					return rs.getInt("coins");
 			return -1;
 		} catch (Exception e) {
-
+			
 		}
 		return -1;
 	}
-
+	
 	public int getCoinDays(int id) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT day FROM bout_items WHERE id=" + id + " LIMIT 1");
@@ -109,11 +109,11 @@ public class ItemClass {
 				return rs.getInt("day");
 			return -1;
 		} catch (Exception e) {
-
+			
 		}
 		return -1;
 	}
-
+	
 	public String getItemScript(int id) {
 		try {
 			final ResultSet rs = SQLDatabase.doquery("SELECT script FROM bout_items WHERE id=" + id + " LIMIT 1");
@@ -121,15 +121,15 @@ public class ItemClass {
 				return rs.getString("script");
 			return null;
 		} catch (Exception e) {
-
+			
 		}
 		return null;
 	}
-
+	
 	public ResultSet getItemInfo(int id) {
 		final ResultSet rs = SQLDatabase
 				.doquery("SELECT reqlevel, bot, part FROM bout_items WHERE id=" + id + " LIMIT 1");
 		return rs;
 	}
-
+	
 }
