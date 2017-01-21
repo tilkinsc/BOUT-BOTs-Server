fa58cad77a41cdb42399b6b312655637ffab4c2a
bud <bud@BudPc>
2017-01-21 00:20:05 -0500

    changed from static to mutable to be make a thread for extendability

--- BOTS Game Server/GameServer/src/loginserver/ChannelServer.java
+++ BOTS Game Server/GameServer/src/loginserver/ChannelServer.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 
 import shared.SQLDatabase;
 
-class ChannelServer {
+class ChannelServer extends Thread {
 
 	// Packets
 	public static final byte[] CHANNEL_HEADERBYTE = { (byte) 0xEE, (byte) 0x2C, (byte) 0x50, (byte) 0x01 };
@@ -21,21 +21,21 @@ class ChannelServer {
 	public static final byte[] NULLBYTE = { (byte) 0x00 };
 
 	// Channel vars
-	public static String[] channel_detail = new String[12];
-	public static int[] channel_id = new int[12];
-	public static String[] channel_name = new String[12];
-	public static int[] channel_namelength = new int[12];
-	public static int[] channel_min = new int[12];
-	public static int[] channel_max = new int[12];
-	public static int[] channel_players = new int[12];
-	public static int channel_i = 0;
+	public String[] channel_detail = new String[12];
+	public int[] channel_id = new int[12];
+	public String[] channel_name = new String[12];
+	public int[] channel_namelength = new int[12];
+	public int[] channel_min = new int[12];
+	public int[] channel_max = new int[12];
+	public int[] channel_players = new int[12];
+	public int channel_i = 0;
 
 	// SQL Querys
 	protected static final String GET_CHANNEL_QUERY = "SELECT * FROM bout_channels WHERE status=1 LIMIT 12";
 
-	private static boolean stop = false;
+	private boolean stop = false;
 	
-	protected static void getChannels() {
+	protected void getChannels() {
 
 		try {
 			final ResultSet rs = SQLDatabase.doquery(GET_CHANNEL_QUERY);
@@ -71,58 +71,62 @@ class ChannelServer {
 		}
 	}
 
-	public static void main() throws Exception {
+	public void run() {
 		final byte[] receiveData = new byte[12];
 		byte[] sendData = new byte[340];
 		String reqString;
 		int port;
 		InetAddress IPAddress;
 		
-		final DatagramSocket serverSocket = new DatagramSocket(11010);
-		serverSocket.setSoTimeout(5000);
-		DatagramPacket sendPacket;
-		Main.logger.log("ChannelList", "Channel List is walking on " + 11010);
+		try {
+			final DatagramSocket serverSocket = new DatagramSocket(11010);
+			serverSocket.setSoTimeout(5000);
+			DatagramPacket sendPacket;
+			Main.logger.log("ChannelList", "Channel List is walking on " + 11010);
+			
+			while (!stop) {
+				final DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				try {
+					serverSocket.receive(receivePacket);
+				} catch (SocketTimeoutException e) {
+					continue;
+				}
+				reqString = new String(receivePacket.getData(), "ISO8859-1");
+				if (reqString.startsWith("\u00FA\u002A\u0002")) {
+					Main.logger.log("ChannelList", "Channel List Requested.");
+					getChannels();
 		
-		while (!stop) {
-			final DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			try {
-				serverSocket.receive(receivePacket);
-			} catch (SocketTimeoutException e) {
-				continue;
+					// make packet
+					String channel_packet = new String(CHANNEL_HEADERBYTE, "ISO8859-1");
+					for (int i = 0; i < channel_i; i++)
+						channel_packet += channel_detail[i];
+		
+					for (int i = 0; i < 12 - channel_i; i++)
+						channel_packet += new String(CHANNEL_EMPTY, "ISO8859-1");
+		
+					for (int i = 0; i < 4; i++)
+						channel_packet += new String(CHANNEL_FOOTER, "ISO8859-1");
+		
+					sendData = channel_packet.getBytes("ISO8859-1");
+		
+					IPAddress = receivePacket.getAddress();
+		
+					port = receivePacket.getPort();
+		
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+		
+					serverSocket.send(sendPacket);
+					Main.logger.log("ChannelList", "Sent Channels");
+				}
 			}
-			reqString = new String(receivePacket.getData(), "ISO8859-1");
-			if (reqString.startsWith("\u00FA\u002A\u0002")) {
-				Main.logger.log("ChannelList", "Channel List Requested.");
-				getChannels();
-
-				// make packet
-				String channel_packet = new String(CHANNEL_HEADERBYTE, "ISO8859-1");
-				for (int i = 0; i < channel_i; i++)
-					channel_packet += channel_detail[i];
-
-				for (int i = 0; i < 12 - channel_i; i++)
-					channel_packet += new String(CHANNEL_EMPTY, "ISO8859-1");
-
-				for (int i = 0; i < 4; i++)
-					channel_packet += new String(CHANNEL_FOOTER, "ISO8859-1");
-
-				sendData = channel_packet.getBytes("ISO8859-1");
-
-				IPAddress = receivePacket.getAddress();
-
-				port = receivePacket.getPort();
-
-				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-
-				serverSocket.send(sendPacket);
-				Main.logger.log("ChannelList", "Sent Channels");
-			}
+			serverSocket.close();
+		} catch(Exception e) {
+			
 		}
-		serverSocket.close();
 		System.out.println("Ended");
 	}
 	
-	public static void stopThread() {
+	public void stopThread() {
 		stop = true;
 	}
 	
