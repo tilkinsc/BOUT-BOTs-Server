0ba3e9cc618bbf425867598537618ff98c1b9b00
bud <bud@BudPc>
2017-01-25 18:17:39 -0500

    cleaned up spacing holes and made current with Util class

--- BOTS Game Server/GameServer/src/loginserver/RoomUDPServer.java
+++ BOTS Game Server/GameServer/src/loginserver/RoomUDPServer.java
@@ -6,6 +6,7 @@ import java.net.InetAddress;
 import java.net.SocketTimeoutException;
 
 import shared.SQLDatabase;
+import shared.Util;
 
 public class RoomUDPServer extends Thread {
 
@@ -33,7 +34,6 @@ public class RoomUDPServer extends Thread {
 			
 			while (!stop) {
 				final DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-				
 				try {
 					socket.receive(packet);
 				} catch (SocketTimeoutException e) {
@@ -44,15 +44,13 @@ public class RoomUDPServer extends Thread {
 				final int port = packet.getPort();
 				final byte[] data = packet.getData();
 
-				final String datan = new String(data, "ISO8859-1");
+				final String datan = Util.isoString(data);
 				if (datan.startsWith("\u00C9\u0000")) {
 					Main.logger.log("RoomUDPServer", "Save port " + port + " of IP " + address.toString().substring(1));
 					SQLDatabase.doupdate("UPDATE `rooms` SET `port`=" + port + " WHERE `ip`='"
 							+ address.toString().substring(1) + "' AND `port`=0");
 				}
-
 			}
-
 			socket.close();
 		} catch (Exception e) {
 			Main.logger.log("Exception", e.getMessage());
