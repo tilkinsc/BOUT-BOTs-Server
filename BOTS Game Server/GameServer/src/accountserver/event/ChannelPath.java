61daab99fd731c6552969711426e7adf287890c3
bud <bud@BudPc>
2017-02-03 23:46:13 -0500

    ChannelServer>Channel, ServerPath added, ChannelPath added
    ServerPath is a revision of the renamed ClientPath. Its purpose is to be
    abstract. Channel became static and all functionality went into
    ChannelPath for the better. I will be doing this with the other servers.
    It was a frequent layout I didn't want to mess with. It is more
    organized this way too. More static, less needless instantiation.

---
+++ BOTS Game Server/GameServer/src/accountserver/event/ChannelPath.java
@@ -0,0 +1,72 @@
+package accountserver.event;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+
+import accountserver.Channel;
+import accountserver.Main;
+import shared.Util;
+
+public class ChannelPath extends ServerPath {
+
+	
+	public ChannelPath(int port, int timeout) {
+		super(port, timeout);
+	}
+
+	@Override
+	public void run() {
+		final byte[] receiveData = new byte[12];
+		byte[] sendData = new byte[340];
+		String reqString;
+		int client_port;
+		InetAddress IPAddress;
+		
+		try {
+			final DatagramSocket serverSocket = new DatagramSocket(port);
+			serverSocket.setSoTimeout(timeout);
+			DatagramPacket sendPacket;
+			Main.logger.log("ChannelList", "Channel List is walking on " + port);
+			
+			while (!stop) {
+				final DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				try {
+					serverSocket.receive(receivePacket);
+				} catch (SocketTimeoutException e) {
+					continue;
+				}
+				
+				reqString = Util.isoString(receivePacket.getData());
+				if (reqString.startsWith("\u00FA\u002A\u0002")) {
+					Main.logger.log("ChannelList", "Channel List Requested.");
+					Channel.getChannels(); // probably could add a 'channel list updated' to avoid getting over and over and over again
+					
+					String channel_packet = Util.isoString(Channel.CHANNEL_HEADERBYTE);
+					for (int i = 0; i < Channel.channel_i; i++)
+						channel_packet += Channel.channel_detail[i];
+					
+					for (int i = 0; i < 12 - Channel.channel_i; i++)
+						channel_packet += Util.isoString(Channel.CHANNEL_EMPTY);
+					
+					for (int i = 0; i < 4; i++)
+						channel_packet += Util.isoString(Channel.CHANNEL_FOOTER);
+					
+					sendData = channel_packet.getBytes("ISO8859-1");
+					IPAddress = receivePacket.getAddress();
+					client_port = receivePacket.getPort();
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, client_port);
+					serverSocket.send(sendPacket);
+					
+					Main.logger.log("ChannelList", "Sent Channels");
+				}
+			}
+			serverSocket.close();
+		} catch(Exception e) {
+			Main.logger.log("Exception", e.getMessage());
+		}
+		System.out.println("Ended");
+	}
+
+}
